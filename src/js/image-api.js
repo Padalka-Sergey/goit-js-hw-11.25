5920d88eaf025eb881fe7aeca0d555e7a7d55001
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-03-23 23:43:11 +0200

    request

--- src/js/image-api.js
+++ src/js/image-api.js
@@ -1,18 +1,19 @@
 import axios from 'axios';
 
 const API_KEY = '31316386-df3d7a07dab36b9800dfb8d2b';
-const PARAMS = '&image_type=photo&orientation=horisontal&savesearch=true';
+const PARAMS =
+  '&image_type=photo&orientation=horisontal&savesearch=true&per_page=40';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
-function fetchFotos(query) {
+function fetchFotos(query, page) {
   return axios
-    .get(`?key=${API_KEY}&q=${query}${PARAMS}`)
-    .then(({ data }) => data)
-    .catch(error => {
-      console.log(error);
-      console.log('ОШИБКА');
-    });
+    .get(`?key=${API_KEY}&q=${query}${PARAMS}&page=${page}`)
+    .then(({ data }) => data);
+  // .catch(error => {
+  //   console.log(error);
+  //   console.log('ОШИБКА');
+  // });
 }
 
 export { fetchFotos };
--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,90 @@
 import { fetchFotos } from './image-api';
 
 const refForm = document.querySelector('#search-form');
+const refGallery = document.querySelector('.gallery');
+const refLoadMore = document.querySelector('.load-more');
+let searchQuery = null;
+let page = 1;
 
 refForm.addEventListener('submit', onSubmit);
+refLoadMore.addEventListener('click', onClckloadMore);
 
 function onSubmit(evt) {
   evt.preventDefault();
   const form = evt.target;
-  const searchQuery = form.elements.searchQuery.value;
+  searchQuery = form.elements.searchQuery.value;
   if (searchQuery === '') {
     return console.log('Please fill in all the fields!');
   }
-  console.log(searchQuery);
-  fetchFotos(searchQuery).then(data => {
-    if (data.hits.length === 0) {
-      return console.log(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-    console.log(data.hits);
-  });
 
-  form.reset();
+  fetchFotos(searchQuery, page)
+    .then(({ hits, totalHits }) => {
+      if (hits.length === 0) {
+        return console.log(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+
+      showLoadMore(hits, totalHits, page);
+      makeMarkup(hits);
+      page += 1;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function makeMarkup(dataResp) {
+  const markup = dataResp
+    .map(
+      dataEl => `<div class="photo-card">
+  <img src=${dataEl.webformatURL} alt=${dataEl.tags} loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes ${dataEl.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views ${dataEl.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments ${dataEl.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads ${dataEl.downloads}</b>
+    </p>
+  </div>
+</div>`
+    )
+    .join('');
+  refGallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function onClckloadMore() {
+  fetchFotos(searchQuery, page)
+    .then(({ hits, totalHits }) => {
+      showLoadMore(hits, totalHits, page);
+      makeMarkup(hits);
+      page += 1;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+function showLoadMore(hits, totalHits, page) {
+  console.log(page);
+  console.log(hits.length * page);
+  console.log(totalHits);
+  if (hits.length % 40 === 0 && hits.length * page < totalHits) {
+    console.log('Первый иф');
+    // console.log(' Второй иф');
+    return refLoadMore.classList.add('load-more-visible');
+  }
+  if (hits.length * page >= totalHits - 40) {
+    // console.log('Первый иф');
+    console.log('Второй иф');
+    return refLoadMore.classList.remove('load-more-visible');
+  }
+  //   refLoadMore.classList.remove('load-more-visible');
+  //   console.log('Третий');
 }
