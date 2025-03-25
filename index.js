fb0811fbf5a1fc8733d44a5891bee409b45661b4
Padalka-Sergey <106059712+Padalka-Sergey@users.noreply.github.com>
2025-03-25 20:52:35 +0000

    Deploying to gh-pages from @ Padalka-Sergey/goit-js-hw-11.25@23cae63376b3c543a7ba9efeaf74b0f417d4e3b0 🚀

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import{a as f}from"./assets/vendor-C19taMLP.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))l(r);new MutationObserver(r=>{for(const n of r)if(n.type==="childList")for(const i of n.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&l(i)}).observe(document,{childList:!0,subtree:!0});function e(r){const n={};return r.integrity&&(n.integrity=r.integrity),r.referrerPolicy&&(n.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?n.credentials="include":r.crossOrigin==="anonymous"?n.credentials="omit":n.credentials="same-origin",n}function l(r){if(r.ep)return;r.ep=!0;const n=e(r);fetch(r.href,n)}})();const p="31316386-df3d7a07dab36b9800dfb8d2b",g="&image_type=photo&orientation=horisontal&savesearch=true&per_page=40";f.defaults.baseURL="https://pixabay.com/api/";f.defaults.headers.post["Content-Type"]="application/json";function u(o,t){return f.get(`?key=${p}&q=${o}${g}&page=${t}`).then(({data:e})=>e)}const h=document.querySelector("#search-form"),y=document.querySelector(".gallery"),a=document.querySelector(".load-more");let c=null,s=1;h.addEventListener("submit",b);a.addEventListener("click",L);function b(o){if(o.preventDefault(),c=o.target.elements.searchQuery.value,c==="")return console.log("Please fill in all the fields!");u(c,s).then(({hits:e,totalHits:l})=>{if(e.length===0)return console.log("Sorry, there are no images matching your search query. Please try again.");m(e,l,s),d(e),s+=1}).catch(e=>{console.log(e)})}function d(o){const t=o.map(e=>`<div class="photo-card">
+import{a as f}from"./assets/vendor-C19taMLP.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const i of t)if(i.type==="childList")for(const c of i.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function e(t){const i={};return t.integrity&&(i.integrity=t.integrity),t.referrerPolicy&&(i.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?i.credentials="include":t.crossOrigin==="anonymous"?i.credentials="omit":i.credentials="same-origin",i}function a(t){if(t.ep)return;t.ep=!0;const i=e(t);fetch(t.href,i)}})();const y="31316386-df3d7a07dab36b9800dfb8d2b",h="&image_type=photo&orientation=horisontal&savesearch=true&per_page=40";f.defaults.baseURL="https://pixabay.com/api/";f.defaults.headers.post["Content-Type"]="application/json";function u(o,r){return f.get(`?key=${y}&q=${o}${h}&page=${r}`).then(({data:e})=>e)}const b=document.querySelector("#search-form"),d=document.querySelector(".gallery"),n=document.querySelector(".load-more");let l=null,s=1;b.addEventListener("submit",g);n.addEventListener("click",L);function g(o){if(o.preventDefault(),l=o.target.elements.searchQuery.value,s=1,v(),M(),l===""){Notiflix.Notify.failure("Please fill in all the fields!");return}u(l,s).then(({hits:e,totalHits:a})=>{if(e.length===0){Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");return}p(e,a,s),m(e),s+=1}).catch(e=>{console.log(e)})}function L(){u(l,s).then(({hits:o,totalHits:r})=>{p(o,r,s),m(o),s+=1}).catch(o=>{console.log(o)})}function m(o){const r=o.map(e=>`<div class="photo-card">
   <img src=${e.webformatURL} alt=${e.tags} loading="lazy" />
   <div class="info">
     <p class="info-item">
-      <b>Likes ${e.likes}</b>
+      <b>Likes</b> ${e.likes}
     </p>
     <p class="info-item">
-      <b>Views ${e.views}</b>
+      <b>Views</b> ${e.views}
     </p>
     <p class="info-item">
-      <b>Comments ${e.comments}</b>
+      <b>Comments</b> ${e.comments}
     </p>
     <p class="info-item">
-      <b>Downloads ${e.downloads}</b>
+      <b>Downloads</b> ${e.downloads}
     </p>
   </div>
-</div>`).join("");y.insertAdjacentHTML("beforeend",t)}function L(){u(c,s).then(({hits:o,totalHits:t})=>{m(o,t,s),d(o),s+=1}).catch(o=>{console.log(o)})}function m(o,t,e){if(console.log(e),console.log(o.length*e),console.log(t),o.length%40===0&&o.length*e<t)return console.log("Первый иф"),a.classList.add("load-more-visible");if(o.length*e>=t-40)return console.log("Второй иф"),a.classList.remove("load-more-visible")}
+</div>`).join("");d.insertAdjacentHTML("beforeend",r)}function p(o,r,e){if(o.length%40===0&&o.length*e<r)return n.classList.add("load-more-visible");n.classList.remove("load-more-visible"),Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.")}function v(){d.innerHTML=""}function M(){n.classList.contains("load-more-visible")&&n.classList.remove("load-more-visible")}
 //# sourceMappingURL=index.js.map
